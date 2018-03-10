9f34c13545024d857638d2ee2852ff7b9876703d
Owen Harvey <owenlharvey@gmail.com>
2018-03-10 15:36:32 +1000

    Cleaning up

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import           Control.Applicative
+import           Control.Concurrent
 import           Control.Concurrent.Async
 import           Control.Exception
 import           Control.Monad
@@ -15,14 +16,11 @@ main = concurrently_
   (connect "localhost" "30005" (streamHosts "30004")) -- Beast messages
 
 streamHosts :: ServiceName -> (Socket, SockAddr) -> IO ()
-streamHosts p (s, _) = runConcurrently $ traverse_ (streamConcurrent s p) ips
-
-streamConcurrent :: Socket -> ServiceName -> HostName -> Concurrently ()
-streamConcurrent s p h = Concurrently $ catch (stream s p h) err
-  where
-    err :: SomeException -> IO ()
-    err = const $ pure ()
+streamHosts p (s, _) = runConcurrently $ traverse_ (Concurrently . stream s p) ips
 
 stream :: Socket -> ServiceName -> HostName -> IO ()
-stream s p h = connect h p $ \(s', _) ->
-  forever $ recv s' 10000 >>= maybe (pure ()) (send s)
+stream s p h = catch (connect h p f) err
+  where
+    f (s', _) = forever $ recv s' 10000 >>= maybe (pure ()) (send s)
+    err :: SomeException -> IO ()
+    err = const $ threadDelay (60 * 1000 * 1000) >> stream s p h
